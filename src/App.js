10f0da0dae76cd51e45de1ab49d9554425a9f5f8
LuDeOliv <ludeoliv@ie.ibm.com>
2017-04-22 14:45:44 +0100

    fix #4 fix #7

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from '../images/carpoolinglogo.png';
+import driverimage from '../images/schools.svg';
+import poolerimage from '../images/poolinG.png';
 import './App.css';
+//import Login from './modules/login';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
@@ -8,11 +12,15 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to IrelandPooling</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <h1>Please define what are you</h1>
+          <ul role="nav">
+            <Link to="/driver"><img src={driverimage} alt="logo" height="80" width="94"/></Link>
+            <Link to="/pooler"><img src={poolerimage} alt="logo" height="80" width="94"/></Link>
+          </ul>
+       </div>
       </div>
     );
   }
---
+++ src/modules/Repos.js
@@ -0,0 +1,11 @@
+// modules/Repos.js
+import React, { Component } from 'react';
+
+
+class Repos extends Component{
+  render() {
+    return <div>Repos</div>
+  }
+}
+
+export default Repos;
