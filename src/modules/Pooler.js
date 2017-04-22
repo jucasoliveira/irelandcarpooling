d9ba1e1df1a766c8ae5ee3e6ca92ce4e79153caa
Igor Souza <igfasouza@gmailcom>
2017-04-22 17:47:29 +0100

    “form”

--- src/modules/Pooler.js
+++ src/modules/Pooler.js
@@ -4,6 +4,24 @@ import logo from '../../images/carpoolinglogo.png';
 
 
 class Pooler extends Component{
+
+    constructor(props){
+      super(props);
+      this.state={
+        value:""
+      };
+
+    }
+
+    myfunction =(event)=>{
+      //this.setState({value: event.target.value});
+      alert(this.state.value);
+    }
+
+    myfunctionInput =(event)=>{
+      this.setState({value: event.target.value});
+    }
+
     render() {
     return (
       <div className="App">
@@ -12,6 +30,14 @@ class Pooler extends Component{
           <h2>Please define where you want to go</h2>
         </div>
         <div>
+
+        <form onSubmit={this.myfunction}>
+          <label>
+            Name:
+            <input type="text" name="name" value={this.state.value} onChange={this.myfunctionInput}/>
+          </label>
+          <input type="submit" value="OK"  />
+        </form>
 
        </div>
       </div>
