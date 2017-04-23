f3b5d1f88ea0e547061e32f4e60d865495fd08d9
LuDeOliv <ludeoliv@ie.ibm.com>
2017-04-23 14:33:33 +0100

    mudancas em geral #10

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import logo from '../images/carpoolinglogo.png';
+import logo from '../images/logo.svg';
 import driverimage from '../images/schools.svg';
-import poolerimage from '../images/poolinG.png';
+import poolerimage from '../images/hailo.png';
 import './styles/App.css';
 //import Login from './modules/login';
 import { Link } from 'react-router-dom';
@@ -15,10 +15,10 @@ class App extends Component {
           <h2>Welcome to IrelandPooling</h2>
         </div>
         <div>
-          <h1>Please define what are you</h1>
+          <h1>Please define who are you :)</h1>
           <ul role="nav">
-            <Link to="/driver"><img src={driverimage} alt="logo" height="80" width="94"/></Link>
-            <Link to="/pooler"><img src={poolerimage} alt="logo" height="80" width="94"/></Link>
+            <Link to="/driver"><img src={driverimage} alt="logo" height="80" width="94" align="center"/></Link>
+            <Link to="/pooler"><img src={poolerimage} alt="logo" height="80" width="58" align="center"/></Link>
           </ul>
        </div>
       </div>
